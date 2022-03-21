5d4a806b4bfbee73dc53e065c8bb3f451cebe9a2
Chompiras15 <99057061+Chompiras15@users.noreply.github.com>
2022-03-21 12:51:47 -0500

    delete

--- delete_categorie.php
+++ delete_categorie.php
@@ -12,16 +12,28 @@
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
   page_require_level(1);
+  $tableActi = "";
+  $SuperUser = current_user();
+  if( $SuperUser["sede"]=="T-Chimbote"){ $tableActi="activity_tasachim";$tabla_sed="sede_tasachimbote";}
+  if( $SuperUser["sede"]=="T-Samanco") {$tableActi="activity_samanco";$tabla_sed="sede_samanco";}
+  if( $SuperUser["sede"]=="T-Supe") {$tableActi="activity_supe";$tabla_sed="sede_supe";}
+  if( $SuperUser["sede"]=="T-Vegueta"){ $tableActi="activity_vegueta";$tabla_sed="sede_vegueta";}
+  if( $SuperUser["sede"]=="T-Callao") {$tableActi="activity_callao";$tabla_sed="sede_callao";}
+  if( $SuperUser["sede"]=="T-Pisco") {$tableActi="activity_pisco";$tabla_sed="sede_pisco";}
+  if( $SuperUser["sede"]=="T-Atico") {$tableActi="activity_atico";$tabla_sed="sede_atico";}
+  if( $SuperUser["sede"]=="T-Matarani") {$tableActi="activity_matarani";$tabla_sed="sede_matarani";}
+  if( $SuperUser["sede"]=="E-Chimbote") {$tableActi="activity_exalmar_chim";$tabla_sed="sede_exalmar_chim";}
+  if( $SuperUser["sede"]=="E-Chicama") {$tableActi="activity_exalmar_mala";$tabla_sed="sede_exalmar_mala";}
 ?>
 <?php
-  $categorie = find_by_id('sede_tasachimbote',(int)$_GET['id']);
+  $categorie = find_by_id($tabla_sed,(int)$_GET['id']);
   if(!$categorie){
     $session->msg("d","ID de la ruma falta.");
     redirect('categorie.php');
   }
 ?>
 <?php
-  $delete_id = delete_by_id('sede_tasachimbote',(int)$categorie['id']);
+  $delete_id = delete_by_id($tabla_sed,(int)$categorie['id']);
   if($delete_id){
       $session->msg("s","Ruma eliminada");
       redirect('categorie.php');
