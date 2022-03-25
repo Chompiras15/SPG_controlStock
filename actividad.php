561468f8a3178918e93e5a1e998867001c0afc5e
Chompiras15 <99057061+Chompiras15@users.noreply.github.com>
2022-03-25 17:26:22 -0500

    cf

--- actividad.php
+++ actividad.php
@@ -77,7 +77,7 @@
      </div>
   </div>
    <div class="row">
-    <div class="col-md-10">
+    <div class="col-md-12">
     <div class="panel panel-default">
       <div class="panel-heading">
         <strong>
