635ba1bb9e993f3b08c9cf7a74b5444ca8bd4db3
Chompiras15 <99057061+Chompiras15@users.noreply.github.com>
2022-03-25 19:31:03 -0500

    change

--- add_categorie.php
+++ add_categorie.php
@@ -123,7 +123,7 @@
         <div class="col-md-3">
         </div>
         <div class="col-md-6 cont_form">
-          <form method="post" action="categorie.php">
+          <form method="post" action="add_categorie.php">
           <div class="material-textfield">
               <input placeholder=" " type="text" name="sector" required>
               <label>Sector</label>
