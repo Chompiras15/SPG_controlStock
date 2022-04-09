3d35dba750c932810b36ed1d958d6a664b2681e5
Jhonatan Lara <32351185+JhonLaraS@users.noreply.github.com>
2022-04-09 14:16:41 -0500

    tabla temperatura

--- actividad.php
+++ actividad.php
@@ -16,12 +16,13 @@
   page_require_level(4); // verifica el nivel de usuario 
 
   $tableActi = "";
+  $tableTemp = "";
   $SuperUser = current_user();
   if( $SuperUser["sede"]=="T-Chimb"){ $tableActi="activity_tasachim";$tabla_sed="sede_tasachimbote";}
   if( $SuperUser["sede"]=="T-Samanco") {$tableActi="activity_samanco";$tabla_sed="sede_samanco";}
   if( $SuperUser["sede"]=="T-Supe") {$tableActi="activity_supe";$tabla_sed="sede_supe";}
   if( $SuperUser["sede"]=="T-Vegueta"){ $tableActi="activity_vegueta";$tabla_sed="sede_vegueta";}
-  if( $SuperUser["sede"]=="T-Callao") {$tableActi="activity_callao";$tabla_sed="sede_callao";}
+  if( $SuperUser["sede"]=="T-Callao") {$tableActi="activity_callao";$tableTemp="temp_callao";$tabla_sed="sede_callao";}
   if( $SuperUser["sede"]=="T-Pisco") {$tableActi="activity_pisco";$tabla_sed="sede_pisco";}
   if( $SuperUser["sede"]=="T-Atico") {$tableActi="activity_atico";$tabla_sed="sede_atico";}
   if( $SuperUser["sede"]=="T-Matarani") {$tableActi="activity_matarani";$tabla_sed="sede_matarani";}
