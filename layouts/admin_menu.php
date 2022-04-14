ba13cd389ae299133c5d7da18cb13ad4103cb633
Chompiras15 <99057061+Chompiras15@users.noreply.github.com>
2022-04-14 11:12:25 -0500

    xs

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -10,5 +10,5 @@
   <li><a href="#" class="submenu-toggle"><i class="glyphicon glyphicon-th-large"></i><span>Actividades</span></a>
     <ul class="nav submenu"><li><a href="actividad.php">Administrar Actividades</a> </li><li><a href="monthly_sales.php">Reporte Diario</a></li><li><a href="daily_sales.php">Reporte Mensual</a></li><li><a href="sales_report.php">Reporte Rangos </a></li></ul>
   </li>
-  <?php if( $SuperUser["sede"]=="T-Callao" || $SuperUser["sede"]=="E-Chimbote") {?><li><a href="temperature.php"><i class="glyphicon glyphicon-filter"></i><span>Temperaturas</span></a></li><?php } ?>
+  <?php if( $SuperUser["sede"]=="T-Callao" || $SuperUser["sede"]=="E-Chicama") {?><li><a href="temperature.php"><i class="glyphicon glyphicon-filter"></i><span>Temperaturas</span></a></li><?php } ?>
 </ul>
